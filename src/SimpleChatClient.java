f13655a65d8f25008b2d3e1fb5bd2672d8b5fbd4
rcp-sergey <rcp.sergey@gmail.com>
2017-09-19 23:57:18 +0300

    Committed to push

--- src/SimpleChatClient.java
+++ src/SimpleChatClient.java
@@ -1,4 +1,89 @@
 package PACKAGE_NAME;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+
 public class SimpleChatClient {
+
+    JTextArea incoming;
+    JTextField outgoing;
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket sock;
+
+    public static void main(String[] args) {
+        SimpleChatClient client = new SimpleChatClient();
+        client.go();
+    }
+
+    public void go() {
+
+        JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        incoming = new JTextArea(15, 50);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        outgoing = new JTextField(20);
+
+        JButton sendButon = new JButton("Send");
+        sendButon.addActionListener(new SendButtonListener());
+
+        mainPanel.add(qScroller);
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButon);
+        setUpNetworking();
+
+        Thread readerThread = new Thread(new IncomingReader());
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+    }
+
+    private void setUpNetworking() {
+        try {
+            sock = new Socket("127.0.0.1", 5000);
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            reader = new BufferedReader(streamReader);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("network established");
+        } catch (IOException e) {e.printStackTrace();}
+    }
+
+    public class IncomingReader implements Runnable {
+
+        @Override
+        public void run() {
+            String message;
+            try {
+
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    incoming.append(message + "\n");
+                }
+            } catch (IOException e) {e.printStackTrace();}
+        }
+    }
+
+    public class SendButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                writer.println(outgoing.getText());
+                writer.flush();
+            } catch (Exception ex) {ex.printStackTrace();}
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
+    }
+
 }
